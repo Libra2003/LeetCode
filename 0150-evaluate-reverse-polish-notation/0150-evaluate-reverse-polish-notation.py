91ddcbb594a15bf6d7f179426c40ff2453dedb61
Tayyab Zain <75733607+Libra2003@users.noreply.github.com>
2023-09-13 12:00:14 +0500

    Time: 72 ms (69.63%), Space: 16.9 MB (13.87%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -1,26 +1,17 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
-        stack = []
-        operators = {"+", "-", "/", "*"}
-        val = 0
-        for value in tokens:
-            if value in operators:
-                a = int(stack.pop())
-                b = int(stack.pop())
-
-                if value == "+":
-                    val = b + a
-                elif value == "-":
-                    val = b - a
-                elif value == "*":
-                    val = b * a
-                elif value == "/":
-                    val = b / a
-
-                stack.append(val)
+        operators = {
+            '+': (lambda x, y: operator.add(x, y)),
+            '*': (lambda x, y: operator.mul(x, y)),
+            '/': (lambda x, y: int(x / y)),
+            '-': (lambda x, y: operator.sub(x, y)),
+        }
+        stack = [] # Space: O(n) 
+        for token in tokens: # Time: O(n)
+            if token not in operators:
+                stack.append(int(token))
             else:
-                stack.append(value)
-        if stack:
-            val = int(stack.pop())
-
-        return val
+                x = stack.pop(-2)
+                y = stack.pop()
+                stack.append(operators[token](x, y))
+        return stack[-1]
