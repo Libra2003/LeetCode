504d3d6ebfdc1f890764a17869da90fed9d42f0a
Tayyab Zain <75733607+Libra2003@users.noreply.github.com>
2023-09-29 03:41:14 +0500

    Time: 41 ms (41.98%), Space: 16.2 MB (95.67%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,15 +1,12 @@
-class Solution(object):
-    def isValid(self, s):
-        stack = [] # only use append and pop
-        pairs = {
-            '(': ')',
-            '{': '}',
-            '[': ']'
-        }
-        for bracket in s:
-            if bracket in pairs:
-                stack.append(bracket)
-            elif len(stack) == 0 or bracket != pairs[stack.pop()]:
-                return False
-
-        return len(stack) == 0
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        map = {'(':')', '{': '}', '[':']'}
+        for bracks in s:
+            if bracks in map:
+                stack.append(bracks)
+                continue
+            elif len(stack)==0 or bracks != map[stack.pop()]:
+                    return False
+        return len(stack)==0
+            
